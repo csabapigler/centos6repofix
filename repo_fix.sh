5d1dc50ec9aa0f5eb415026b88ae804dd63f3a47
Pigler Csaba <csabapigler@Pigler-MacBook-Pro.local>
2020-12-25 18:13:59 +0100

    commands abd repo

--- repo_fix.sh
+++ repo_fix.sh
@@ -1,2 +1,13 @@
 #! /usr/bin/bash
 
+cp /etc/yum.repos.d/
+
+cp /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.old
+
+cp CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo
+
+yum clean all
+
+yum -y install ca-certificates openssl nss
+
+yum update
